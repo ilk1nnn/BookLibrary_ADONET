ae89ecc790dda828537cccd8b6f03b5c0b8ffe21
Sule_jr92 <Sule_jr92@STHQ0129-05>
2022-12-04 13:47:00 +0400

    Add project files.

---
+++ BookLibrary_ADONET/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using BookLibrary_ADONET.Commmands;
+using BookLibrary_ADONET.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BookLibrary_ADONET.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public RelayCommand LibrarianCommand { get; set; }
+        public RelayCommand StudentCommand { get; set; }
+
+        public MainWindow mainwindow { get; set; }
+
+
+        public MainWindowViewModel()
+        {
+
+            LibrarianCommand = new RelayCommand(l =>
+            {
+                var lib_window = new LibrarianWindow();
+                lib_window.Show();
+                mainwindow.Close();
+            });
+
+            StudentCommand = new RelayCommand(s =>
+            {
+                var student_window = new StudentWindow();
+                student_window.Show();
+                mainwindow.Close();
+            });
+
+        }
+    }
+}
---
+++ BookLibrary_ADONET/Views/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using BookLibrary_ADONET.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookLibrary_ADONET
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //var app_view_model = new AppViewModel();
+            //app_view_model.mainwindow = this;
+            //app_view_model.StartPrice = StartPriceTxtb;
+            //app_view_model.EndPrice = EndPriceTxtb;
+            //app_view_model.listview = products_listview;
+            //this.DataContext = app_view_model;
+
+            var mw = new MainWindowViewModel();
+            mw.mainwindow = this;
+            this.DataContext = mw;
+
+        }
+    }
+}
